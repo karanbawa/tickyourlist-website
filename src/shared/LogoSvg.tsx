df6a628b4b64ab5b03c277a2b2fcffb0e8a9945f
karanbawa <karan.bawa2@gmail.com>
2024-08-03 22:33:24 +0530

    fix: usesearchparam issue fix

--- src/shared/LogoSvg.tsx
+++ src/shared/LogoSvg.tsx
@@ -8,9 +8,9 @@ const LogoSvg = () => {
       <Image
         src={tyllogo} // The path to your image
         alt="TickYourList logo" // A description for your image
-        width={88} // Set the width to match your SVG's viewBox width
-        height={25} // Set the height to match your SVG's viewBox height
-        layout="intrinsic" // Use intrinsic layout for fixed dimensions
+        width={88} // Set the actual width of the image
+        height={25} // Set the actual height of the image
+        layout="fixed" // Use fixed layout for fixed dimensions
         objectFit="contain" // Ensure the image fits within the container
         className="h-full"
         priority // Prioritize loading of this image
