8ef012aef04aa3e94c310022a1d2f2e077b926aa
karanbawa <karan.bawa2@gmail.com>
2024-08-17 23:20:59 +0530

    feat: clarity integrated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -48,13 +48,19 @@ export default function RootLayout({
             <FooterNav />
             <Footer />
           </AuthProvider>
-          <Script type="text/javascript">
-         {`   (function(c,l,a,r,i,t,y){
+          <Script
+            id="clarity-script"
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+              __html: `
+           (function(c,l,a,r,i,t,y){
               c[a] = c[a] || function () { (c[a].q = c[a].q || []).push(arguments) };
             t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
             y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-    })(window, document, "clarity", "script", "not1sec6kh"); `}
-          </Script>
+    })(window, document, "clarity", "script", "not1sec6kh");
+          `,
+            }}
+          />
         </body>
       </html>
     </GoogleOAuthProvider>
