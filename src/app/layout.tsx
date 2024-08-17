707806075b489d94f7a39f603c3a1763dc974550
karanbawa <karan.bawa2@gmail.com>
2024-08-17 23:06:30 +0530

    feat: clarity integrated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,6 +11,7 @@ import { Metadata } from "next";
 import Head from "next/head";
 import { AuthProvider } from "@/context/AuthContext";
 import { GoogleOAuthProvider } from "@react-oauth/google";
+import Script from 'next/script';
 
 const poppins = Poppins({
   subsets: ["latin"],
@@ -35,20 +36,27 @@ export default function RootLayout({
 }) {
   return (
     <GoogleOAuthProvider clientId={process.env.GOOGLE_CLIENT_ID || ''}>
-    <html lang="en" className={poppins.className}>
-      <Head>
-        <link rel="icon" href="/images/logo/tyllogo.png" /> {/* Add this line */}
-      </Head>
-      <body className="bg-white text-base dark:bg-neutral-900 text-neutral-900 dark:text-neutral-200">
-        <AuthProvider>
-        <ClientCommons />
-        <SiteHeader />
-        {children}
-        <FooterNav />
-        <Footer />
-        </AuthProvider>
-      </body>
-    </html>
+      <html lang="en" className={poppins.className}>
+        <Head>
+          <link rel="icon" href="/images/logo/tyllogo.png" /> {/* Add this line */}
+        </Head>
+        <body className="bg-white text-base dark:bg-neutral-900 text-neutral-900 dark:text-neutral-200">
+          <AuthProvider>
+            <ClientCommons />
+            <SiteHeader />
+            {children}
+            <FooterNav />
+            <Footer />
+          </AuthProvider>
+          <Script type="text/javascript">
+         {`   (function(c,l,a,r,i,t,y){
+              c[a] = c[a] || function () { (c[a].q = c[a].q || []).push(arguments) };
+            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "not1sec6kh"); `}
+          </Script>
+        </body>
+      </html>
     </GoogleOAuthProvider>
   );
 }
