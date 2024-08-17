d606b3b114659e66fcdc6a8c9dc3c00d0b300f94
karanbawa <karan.bawa2@gmail.com>
2024-08-17 17:26:53 +0530

    fix: improved the code quality

--- src/app/(listing-detail)/layout.tsx
+++ src/app/(listing-detail)/layout.tsx
@@ -4,25 +4,15 @@ import BackgroundSection from "@/components/BackgroundSection";
 import ListingImageGallery from "@/components/listing-image-gallery/ListingImageGallery";
 import SectionSliderNewCategories from "@/components/SectionSliderNewCategories";
 import SectionSubscribe2 from "@/components/SectionSubscribe2";
-import { usePathname, useRouter } from "next/navigation";
+import { usePathname } from "next/navigation";
 import React, { ReactNode, Suspense } from "react";
 import MobileFooterSticky from "./(components)/MobileFooterSticky";
 import { imageGallery as listingStayImageGallery } from "./listing-stay-detail/constant";
 import { imageGallery as listingCarImageGallery } from "./listing-car-detail/constant";
 import { imageGallery as listingExperienceImageGallery } from "./listing-experiences-detail/constant";
-import { Route } from "next";
 
 const DetailLayout = ({ children }: { children: ReactNode }) => {
-  const router = useRouter();
   const thisPathname = usePathname();
-  // const searchParams = useSearchParams();
-  // const modal = searchParams?.get("modal");
-
-  // const handleCloseModalImageGallery = () => {
-  //   let params = new URLSearchParams(document.location.search);
-  //   params.delete("modal");
-  //   router.push(`${thisPathname}/?${params.toString()}` as Route);
-  // };
 
   const getImageGalleryListing = () => {
     if (thisPathname?.includes("/listing-stay-detail")) {
@@ -42,8 +32,6 @@ const DetailLayout = ({ children }: { children: ReactNode }) => {
       <div className="ListingDetailPage">
       <Suspense fallback={<div>Loading Listing Image Gallery...</div>}>
         <ListingImageGallery
-          // isShowModal={"PHOTO_TOUR_SCROLLABLE" === "PHOTO_TOUR_SCROLLABLE"}
-          // onClose={handleCloseModalImageGallery}
           images={getImageGalleryListing()}
         />
         </Suspense>
