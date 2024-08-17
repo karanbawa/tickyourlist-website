224ee5f222b4c66ee2f68aa2b5dfa03028d7966c
karanbawa <karan.bawa2@gmail.com>
2024-08-17 23:22:26 +0530

    feat: clarity integrated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -57,7 +57,7 @@ export default function RootLayout({
               c[a] = c[a] || function () { (c[a].q = c[a].q || []).push(arguments) };
             t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
             y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-    })(window, document, "clarity", "script", "not1sec6kh");
+    })(window, document, "clarity", "script", "${process.env.CLARITY_KEY_ID}");
           `,
             }}
           />
