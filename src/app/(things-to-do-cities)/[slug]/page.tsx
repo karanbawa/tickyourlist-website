193fb67a0874b489f12d7356c316079596367354
karanbawa <karan.bawa2@gmail.com>
2024-10-02 09:18:48 +0530

    added currency change based on location

--- src/app/(things-to-do-cities)/[slug]/page.tsx
+++ src/app/(things-to-do-cities)/[slug]/page.tsx
@@ -2,13 +2,13 @@ import { cookies } from 'next/headers';
 import PageHome3 from './PageHome3';
 
 async function getTravelSections(cityCode: string, currency: string) {
-  const res = await fetch(`http://localhost:3005/v1/tyltravelsection/get/public/travel-sections/?cityCode=${cityCode?.toUpperCase()}&domainId=${process.env.WEBSITE_ID}&currency=${currency}`, {
+  const res = await fetch(`${process.env.BASE_URL}/v1/tyltravelsection/get/public/travel-sections/?cityCode=${cityCode?.toUpperCase()}&domainId=${process.env.WEBSITE_ID}&currency=${currency}`, {
     headers: {
       'Content-Type': 'application/json',
       'x-api-key': 'GCMUDiuY5a7WvyUNt9n3QztToSHzK7Uj',
       'Cache-Control': 'no-store, no-cache, must-revalidate, max-age=0',
     },
-    next: { revalidate: 3 }, // Ensure Vercel does not cache this fetch
+    next: { revalidate: 10 }, // Ensure Vercel does not cache this fetch
   });
 
   if (!res.ok) {
