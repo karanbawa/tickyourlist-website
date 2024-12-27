725439192651582dea1063840fe71de08a1dd81e
karanbawa <karan.bawa2@gmail.com>
2024-12-27 12:30:16 +0530

    fix: snowfall additition

--- src/shared/LogoSvg.tsx
+++ src/shared/LogoSvg.tsx
@@ -1,7 +1,6 @@
 import Image from "next/image";
 import React from "react";
-// import tyllogo from "@/images/logos/tyllogo-1.png";
-import tyllogo from "@/images/logos/tyl-logo-xmas.png";
+import tyllogo from "@/images/logos/tyllogo-1.png";
 
 const LogoSvg = () => {
   return (
@@ -9,7 +8,7 @@ const LogoSvg = () => {
       <Image
         src={tyllogo} // The path to your image
         alt="TickYourList logo" // A description for your image
-        width={60} // Set the actual width of the image
+        width={88} // Set the actual width of the image
         height={25} // Set the actual height of the image
         layout="fixed" // Use fixed layout for fixed dimensions
         objectFit="contain" // Ensure the image fits within the container
@@ -19,22 +18,5 @@ const LogoSvg = () => {
     </div>
   );
 };
-
-// const LogoSvg = () => {
-//   return (
-//     <div className="w-full block dark:hidden"> 
-//       <Image
-//         src={tyllogo} // The path to your image
-//         alt="TickYourList logo" // A description for your image
-//         width={88} // Set the actual width of the image
-//         height={25} // Set the actual height of the image
-//         layout="fixed" // Use fixed layout for fixed dimensions
-//         objectFit="contain" // Ensure the image fits within the container
-//         className="h-full"
-//         priority // Prioritize loading of this image
-//       />
-//     </div>
-//   );
-// };
 
 export default LogoSvg;
